1158c3f1e96dfd068af0893a7482db6cc113a140
namth91 <namth2302@gmail.com>
2014-05-09 15:46:05 -0700

    Init Commit

---
+++ android/src/com/atomic/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.atomic.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.atomic.game.AtomicGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AtomicGame(), config);
+	}
+}
---
+++ desktop/src/com/atomic/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.atomic.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.atomic.game.AtomicGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AtomicGame(), config);
+	}
+}
