10583aa568f5deda93ddb452adbea5661a1b74ab
namth91 <namth2302@gmail.com>
2014-05-09 16:16:48 -0700

    Edit DesktopLaucher.java file resizeable width height and title

--- desktop/src/com/atomic/game/desktop/DesktopLauncher.java
+++ desktop/src/com/atomic/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.atomic.game.AtomicGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Atomic Game";
+		config.resizable = false;
+		config.width = 870;
+		config.height = 520;
 		new LwjglApplication(new AtomicGame(), config);
 	}
 }
