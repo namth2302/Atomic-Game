de1add94b4b0abc928b91b6354c7d8462a66be96
namth91 <namth2302@gmail.com>
2014-05-12 16:15:36 -0700

    Ver1

--- core/src/com/atomic/game/AtomicGame.java
+++ core/src/com/atomic/game/AtomicGame.java
@@ -1,27 +1,20 @@
 package com.atomic.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.atomic.option.ASSET;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 
-public class AtomicGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class AtomicGame extends Game implements ApplicationListener {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		ASSET.loadAllFile();
+		setScreen(new ScreenGame(this, "MAIN GAME SCREEN"));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void resume() {
+		super.resume();
+//		ASSET.loadAllFile();
 	}
 }
